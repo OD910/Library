97df93c834c1a37fd18a064efe7352584cbd0a34
OD910 <doszhan.ondasyn@mail.ru>
2025-09-22 22:53:41 +0500

    Book and BorrowingRecord getter setter constructor

--- libraryJavaCore/src/Book.java
+++ libraryJavaCore/src/Book.java
@@ -9,4 +9,82 @@ public class Book {
     private int availableCopies;
 
 
+    public Book(String isbn, String title, Author author, int publicationYear, Genre genre, int totalCopies, int availableCopies) {
+    	this.isbn = isbn;
+    	this.title = title;
+    	this.author = author;
+    	this.publicationYear = publicationYear;
+    	this.genre = genre;
+    	this.totalCopies = totalCopies;
+    	this.availableCopies = availableCopies;
+    }
+
+    public Book() {
+
+    }
+
+
+
+    public String getIsbn() {
+    	return isbn;
+    }
+
+    public void setIsbn(String newIsbn) {
+    	this.isbn = newIsbn;
+    }
+
+    public String getTitle() {
+    	return title;
+    	}
+
+    public void setTitle(String newTitle) {
+    	this.title = newTitle;
+    }
+
+
+    public Author getAuthor() {
+    	return author;
+    }
+
+    public void setAuthor(Author newAuthor) {
+    	this.author = newAuthor;
+    }
+
+    public int getPublicationYear() {
+    	return publicationYear;
+    }
+
+    public void setPublicationYear(int newPublicationYear) {
+    	this.publicationYear = newPublicationYear;
+    }
+
+    public Genre getGenre() {
+    	return genre;
+    }
+
+    public void setGenre(Genre newGenre) {
+    	this.genre = newGenre;
+    }
+
+
+    public int getTotalCopies() {
+    	return totalCopies;
+    }
+
+    public void setTotalCopies(int newTotalCopies) {
+    	this.totalCopies = newTotalCopies;
+    }
+
+
+    public int getAvailableCopies() {
+    	return availableCopies;
+    }
+
+    public void setAvailableCopies(int newAvailableCopies) {
+    	this.availableCopies = newAvailableCopies;
+    }
+
+
+
+
 }
